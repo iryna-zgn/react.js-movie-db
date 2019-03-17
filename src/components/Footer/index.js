dd204b41bf5e2464ef37d832c35d03c6a7eb367e
dmytruk <iryna.zgn@gmail.com>
2019-03-18 00:33:14 +0200

    Move Links to Header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import Logo from './../../components/Logo'
-import SocLinks from './../../components/SocLinks'
-import { RHO_BLOG } from './../../paths'
 
 export default function footer() {
     return (
         <footer className='footer'>
-            <div className='footer__inline'>
-                <SocLinks/>
-                <Logo
-                    link={ RHO_BLOG }
-                    text='blog'
-                    isSmall
-                    isTargetBlank
-                    title='rho blog'/>
-            </div>
             <div className='footer__copyright'>
-                © { (new Date()).getFullYear() }
+                © { (new Date()).getFullYear() } Rho
             </div>
         </footer>
     )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import SearchForm from './../SearchForm'
 import Logo from './../Logo'
+import SocLinks from './../../components/SocLinks'
+import { RHO_BLOG } from './../../paths'
 
 export default function header() {
     return (
         <header className='header'>
-            <Logo
-                text='movies'
-                link='/' />
-            <SearchForm/>
+            <div className='header__main'>
+                <Logo
+                    text='movies'
+                    link='/' />
+                <SearchForm/>
+            </div>
+            <div className='header__links'>
+                <SocLinks/>
+                <Logo
+                    link={ RHO_BLOG }
+                    text='blog'
+                    isSmall
+                    isTargetBlank
+                    title='rho blog'/>
+            </div>
         </header>
     )
 }
